8cfb7396e1c1106ebf0a5d4ac93974e50cdd3ec5
Arvin Lin <al4419@sprite11.doc.ic.ac.uk>
2019-10-18 17:16:20 +0100

    Group decision tree into class

--- decision_tree.py
+++ decision_tree.py
@@ -1,224 +1,129 @@
-"""
-decision_tree.py
-"""
-# Import the required libraries
 import numpy as np
-import matplotlib
 
-# Load the datasets
-filepath = 'wifi_db/'
-filename = 'clean_dataset.txt'
-
-data = np.loadtxt(filepath+filename)
-
-def entropy(data):
+# Define essential functions for training a decision tree
+def entropy(label):
 	"""
-	Usage: H = entropy(dataset)
+	Usage: H = entropy(label)
 	Description: This function calculates the entropy given a data set assuming the last column represents the labels
 	Input: 2d numpy array
 	Output: single value representing the entropy
 	"""
 	
-	labels = set(data[:,-1])
-	data_num = data.shape[0]
+	labels = set(label)
+	label_num = len(label)
 	instances = dict.fromkeys(labels,0)
-	for d in data:
-		instances[d[-1]] += 1
-	prob = [float(k)/data_num for k in list(instances.values())]
+	for l in label:
+		instances[l] += 1
+	prob = [float(k)/label_num for k in list(instances.values())]
 	sep_entropy = [p*np.log2(p) for p in prob]
 	return -sum(sep_entropy)
 
-def find_split(data):
-	"""
-	Usage:
-	Description:
-	Outputs:
-	"""
-	attr_num = data.shape[1] - 1
-	data_num = data.shape[0]
-	info_gain = None
-	node_split = None
-	node_attr = None
-	l_set = np.empty([0, data.shape[1]])
-	r_set = np.empty([0, data.shape[1]])
-	for attr in range(attr_num):
-		# Find the optimal split in this attribute
-		data = data[data[:,attr].argsort()]
-		for i in range(data_num - 1):
-			if data[i, attr] == data[i+1,attr]:
-				continue
-			else:
-				split = (data[i+1, attr] + data[i,attr])/2.0
-				#split the data based on the calculated split
-				b1 = data[data[:,attr] > split]
-				b2 = data[data[:,attr] <= split]
-				# Calculate the information gain with this split
-				# For simplicity we calculate the remainder and take the minus sign to quantify it
-				Hb1 = entropy(b1)
-				Hb2 = entropy(b2)
-				neg_remainder = -(float(b1.shape[0])/data_num*Hb1 + float(b2.shape[0])/data_num*Hb2)
-				if  info_gain == None or neg_remainder > info_gain:
-					info_gain = neg_remainder
-					node_split = split
-					node_attr = attr
-					l_set = b1
-					r_set = b2
-	return node_attr, node_split, l_set, r_set
 
-def decision_tree_learning(sub_data, d = 0):
-	"""
-	Usage:
-	Description: The Tree is represented in a recurrsive dictionary
-	Inputs:
-	Outputs:
-	"""
-	#Create Empty Dictionary
-	node = {'attr':None, 'value':None, 'left':None, 'right':None}
-	#Check if all labels are the same
-	label_num = len(set(sub_data[:,-1]))
-	if label_num == 1:
-		# No further seperation is needed, return the node as leaf
-		node['attr'] = 'leaf'; node['value'] = sub_data[0,-1]
-		return node, d
-	elif label_num == 0:
-		node['attr'] = 'leaf'
-		return node, d		
-	else:
-		attr, split,l_set, r_set = find_split(sub_data)
-		node['attr'] = attr
-		node['split'] = split
-		node['left'], d1 = decision_tree_learning(l_set, d = d+1)
-		node['right'], d2 = decision_tree_learning(r_set, d = d+1)
-		depth = max(d1,d2)
-		return node, depth
-
-#Not Done, in progress
-def prune_1level(tree, data):
-	"""
-	We assume most of the data are classified correctly
-	"""
-	labels = set(data[:,-1])
-	prop_tree = {'data_count':dict.fromkeys(labels,0),'visit_count':0,'left':None, 'right':None}
-	curr_node = prop_tree
-	for data_point in data:
-		true_label = data_point[-1]
-		at_leaf = False
-		label = None
-		curr_node = prop_tree
-		while not at_leaf:
-			attr = tree['attr']
-			curr_node['data_count'][true_label] += 1
-			curr_node['visit_count'] += 1
-			if attr == 'leaf':
-				label = tree['value']
-				at_leaf = True
-			else:
-				split = tree['split']
-				if data_point[attr] > split:
-					if curr_node['left'] == None:
-						next_node = {'data_count':dict.fromkeys(labels,0),'visit_count':0,'left':None, 'right':None}
-						curr_node['left'] = next_node
-						curr_node = next_node
-					else:
-						curr_node = curr_node['left']
-					tree = tree['left']
-				else:
-					if curr_node['right'] == None:
-						next_node = {'data_count':dict.fromkeys(labels,0),'visit_count':0,'left':None, 'right':None}
-						curr_node['left'] = next_node
-						curr_node = next_node
-					else:
-						curr_node = curr_node['right']
-					tree = tree['right']
-	return prop_tree
-
-def classify(tree, data_point):
-	at_leaf = False
-	label = None
-	while not at_leaf:
-		attr = tree['attr']
-		if attr == 'leaf':
-			label = tree['value']
-			at_leaf = True
+# Define the decision tree class
+class node():
+	def __init__(self):
+		self.attr = None
+		self.value = None
+		self.parent = None
+		self.left = None
+		self.right = None
+	def set_parent(self, node_):
+		self.parent = node_
+	def add_child(self, node_, direction):
+		if direction == "left":
+			self.left = node_
+		elif direction == "right":
+			self.right = node_
 		else:
-			split = tree['split']
-			if data_point[attr] > split:
-				tree = tree['left']
-			else:
-				tree = tree['right']
-	return label
+			print("The direction of the child should be either left or right.")
+	def set_attr(self, attr):
+		self.attr = attr
+	def set_value(self, value):
+		self.value = value
+	def clear_child(self):
+		self.left = None
+		self.right = None 
 
-def evaluate(tree, data):
-	data_num = data.shape[0]
-	correct_num = 0
-	for data_point in data:
-		prediction = classify(tree, data_point)
-		correct_num += (prediction == data_point[-1])
-	accuracy = float(correct_num)/data_num
-	return accuracy
+class decision_tree():
+	def __init__(self):
+		self.root = None
+		self.depth = 0
 
+	def find_split(self, data, label):
+		"""
+		Usage:
+		Description:
+		Outputs:
+		"""
+		data_num, attr_num = data.shape
 
-def cross_validate(data,k_fold=10):
-	"""
-	perform k-fold cross validation on learn_decision_tree
-	
-	Args:
-		data (np.array): train/val data with label on the last column
-		k	 (int): data.shape[0]>=k>0
-	Outputs:
-	"""
-	assert(type(data)==np.ndarray)
-	assert(len(data.shape)==2 and data.shape[1]>1)
+		info_gain = float('-inf')
+		node_split = None
+		node_attr = None
+		l_set = np.empty([0, data.shape[1]])
+		r_set = np.empty([0, data.shape[1]])
+		for attr in range(attr_num):
+			# Find the optimal split in this attribute
+			data, label = (data[data[:,attr].argsort()], label[data[:,attr].argsort()])
+			for i in range(data_num - 1):
+				if data[i, attr] == data[i+1,attr]:
+					continue
+				else:
+					split = (data[i+1, attr] + data[i,attr])/2.0
+					#split the data based on the calculated split
+					l_branch = data[:,attr] > split
+					l_data, l_label = (data[l_branch], label[l_branch])
+					r_branch = data[:,attr] <= split
+					r_data, r_label = (data[r_branch], label[r_branch])
+					# Calculate the information gain with this split
+					# For simplicity we calculate the remainder and take the minus sign to quantify it
+					H_l = entropy(l_label)
+					H_r = entropy(r_label)
+					neg_remainder = -float(len(l_label)*H_l + len(r_label)*H_r)/data_num
+					if  neg_remainder > info_gain:
+						info_gain = neg_remainder
+						node_split = split
+						node_attr = attr
+						l_set = (l_data, l_label)
+						r_set = (r_data, r_label)
+		return node_attr, node_split, l_set, r_set
 
-	num_row=data.shape[0]
-	assert(type(k_fold)==int)
-	assert(k_fold>0 and k_fold<=num_row)
+	def decision_tree_learning(self, data, label, d = 0):
+		node_ = node()
+		label_num = len(set(label))
+		if label_num == 1:
+			node_.set_attr('leaf')
+			node_.set_value(label[0])
+			return node_, d
+		else:
+			attr, split, l_set, r_set = self.find_split(data, label)
+			node_.set_attr(attr)
+			node_.set_value(split)
+			node_l, d_l = self.decision_tree_learning(l_set[0], l_set[1], d+1)
+			node_r, d_r = self.decision_tree_learning(r_set[0], r_set[1], d+1)
+			node_l.set_parent(node_); node_r.set_parent(node_)
+			node_.add_child(node_l, "left")
+			node_.add_child(node_r, "right")			
+			d = max(d_l, d_r)
+			return node_, d
 
-	np.random.shuffle(data)
+	def train(self, data, label):
+		self.root, self.depth = self.decision_tree_learning(data, label)
 
-	k_range = lambda k_head,k_tail: (num_row*np.arange(k_head,k_tail)/k_fold).astype(int)
-
-	for k in range(0,k_fold):
-		# divide data to train/val set
-		val_data=data[k_range(k,k+1)]
-		train_data=data[np.concatenate(k_range(0,k),k_range(k+1,k_fold))]
-
-		# train decision tree
-		tree,_=decision_tree_learning(train_data)
-		acc=evaluate(tree,val_data)
-		
-		yield acc
-
-
-def compute_confusion_matrix(tree,test_data,num_label):
-	"""
-	compute confusion matrix of a learnt tree
-	
-	Args:
-		tree	 (dict): learnt decision tree
-		test_data (int): test data with label on the last column
-		num_label (int): number of label category in dataset(row/col number of matrix)
-	Outputs:
-	"""
-	assert(type(tree)==dict)
-	assert(type(test_data)==np.ndarray)
-	assert(len(test_data.shape)==2 and test_data.shape[1]>1)
-	assert(type(num_label)==int)
-	assert(num_label>0)
-
-	confusion_matrix=np.zeros([num_label,num_label])
-
-	for data_point in test_data:
-		predict_label=classify(tree,data_point)
-		gt_label=data_point[-1]
-		confusion_matrix[int(predict_label)-1,int(gt_label)-1]+=1
-
-	return confusion_matrix
-	
-
-def main():
-	tree, _ = decision_tree_learning(data)
-	print(compute_confusion_matrix(tree,data,4))
-
-if __name__=="__main__":
-	main()
+	def classify(self, data):
+		label = []
+		for d in data:
+			tree = self.root
+			at_leaf = False
+			while True:
+				attr = tree.attr
+				if attr == "leaf":
+					label.append(tree.value)
+					break
+				else:
+					split = tree.value
+					if d[attr] > split:
+						tree = tree.left
+					else:
+						tree = tree.right
+		return label
---
+++ train.py
@@ -0,0 +1,60 @@
+from Test import *
+import numpy as np
+
+# Load the datasets
+filepath = ''
+filename = 'clean_dataset.txt'
+data = np.loadtxt(filepath+filename)
+
+x = data[:,:-1]
+y = data[:,-1]
+
+
+def evaluate(predicted_label, true_label):
+	"""
+	Evaluate accuracy on predicted label given true label
+	
+	Args:
+		predicted_label : 
+		true_label      :
+	Outputs: accuracy of the predicted label
+	"""
+	assert(len(predicted_label)==len(true_label))
+	data_num = len(true_label)
+	accuracy = float(sum(predicted_label == true_label))/data_num
+	return accuracy
+
+
+def compute_confusion_matrix(tree,test_data,num_label):
+	"""
+	compute confusion matrix of a learnt tree
+	
+	Args:
+		tree	 (dict): learnt decision tree
+		test_data (int): test data with label on the last column
+		num_label (int): number of label category in dataset(row/col number of matrix)
+	Outputs:
+	"""
+	#assert(type(tree)==dict)
+	assert(type(test_data)==np.ndarray)
+	assert(len(test_data.shape)==2 and test_data.shape[1]>1)
+	assert(type(num_label)==int)
+	assert(num_label>0)
+
+	confusion_matrix=np.zeros([num_label,num_label])
+
+	predict_label = tree.classify(data[:,:-1])
+	gt_label = data[:,-1]
+	for p, g in zip(predict_label, gt_label):
+		confusion_matrix[int(p)-1,int(g)-1]+=1
+
+	return confusion_matrix
+
+tree = decision_tree()
+tree.train(x,y)
+
+y_pred = tree.classify(x)
+print("Accuracy: " + str(evaluate(y_pred, y)))
+
+confusion_matrix = compute_confusion_matrix(tree, data, len(set(y)))
+print(confusion_matrix)
